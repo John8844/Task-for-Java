9cd519a94a5aa3f4cee680a5657b2daf200c6619
Johnson <johnson794544@gmail.com>
2023-07-17 15:04:21 +0530

    Task

--- Shield Task/src/Main.java
+++ Shield Task/src/Main.java
@@ -1,64 +1,65 @@
 import java.util.Scanner;
-import java.lang.String;
-import java.util.ArrayList;
-class avengers{
 
+class Avengers {
 }
-class missions{
-    String missName;
-    public String status;
-    public String aveList;
-    public String details;
-    missions(){
 
+class Missions {
+    private String missName;
+    private String status;
+    private String aveList;
+    private String details;
+    Missions() {
+        display();
     }
-    missions(String aveList, String missName,String details, String status) {
+    Missions(String aveList, String missName, String details, String status) {
         this.missName = missName;
         this.aveList = aveList;
         this.details = details;
         this.status = status;
     }
-    public void display(){
 
-        ArrayList<String> cars = new ArrayList<String>();
-        System.out.println("Mission Name :"+missName);
-        System.out.println("Status :"+status);
-        System.out.println("Avengers List :"+aveList);
-        System.out.println("Mission Details :"+details);
+    public void display() {
+        System.out.println("Mission Name: " + missName);
+        System.out.println("Status: " + status);
+        System.out.println("Avengers List: " + aveList);
+        System.out.println("Mission Details: " + details);
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        for (int i=0;i<15;i++){
-            System.out.println("=======------S.H.I.E.L.D ------=========");
-            System.out.println("");
-            System.out.println("Welcome to Captain Fury. (Note: Below interface is for Captain Fury)");
-            System.out.println("");
-            System.out.println("1. Assign mission to Avengers");
-            System.out.println("2. Check the missions");
-            System.out.println("3. Check mission’s details");
-            System.out.println("4. Check Avenger’s details");
-            System.out.println("5. Update Mission’s status");
-            System.out.println("6. List Avengers");
-            System.out.println("7. Assign avenger to mission.");
-            System.out.println("");
-            System.out.println("Enter the option :");
-            int choice=in.nextInt();
-            switch (choice){
+        Scanner in = new Scanner(System.in);
+        System.out.println("=======------S.H.I.E.L.D ------=========");
+        System.out.println("");
+        System.out.println("Welcome to Captain Fury. (Note: Below interface is for Captain Fury)");
+        System.out.println("");
+        System.out.println("1. Assign mission to Avengers");
+        System.out.println("2. Check the missions");
+        System.out.println("3. Check mission’s details");
+        System.out.println("4. Check Avenger’s details");
+        System.out.println("5. Update Mission’s status");
+        System.out.println("6. List Avengers");
+        System.out.println("7. Assign avenger to mission.");
+        System.out.println("");
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Enter the option: ");
+            int choice = in.nextInt();
+
+            switch (choice) {
                 case 1:
                     System.out.println("Enter Avengers: ");
-                    String avengers=in.next();
+                    String avengers = in.next();
                     System.out.println("Enter Mission Name: ");
-                    String missName=in.next();
+                    String missName = in.next();
                     System.out.println("Enter Mission Details: ");
-                    String missDetails=in.next();
-                    missions arr=new missions(avengers,missName,missDetails,"assigned");
+                    String missDetails = in.next();
+                    Missions obj = new Missions(avengers, missName, missDetails, "assigned");
                     break;
+
                 case 2:
-                    missions arr1=new missions();
-                    arr1.display();
+                    Missions obj1 = new Missions();
+                    break;
             }
         }
     }
+
 }
